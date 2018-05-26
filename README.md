2e276da4b64fa963836f88dac9b75c0921481dfe
Peterpan0927 <1549346936@qq.com>
2018-05-26 21:37:39 +0800

    intercept both Mach traps and BSD syscalls

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 the learing code on High Serria,let's keep learing everyday
 
 > start from chapter 3
+
 > the original website is [newosxbook](newosxbook.com)
---
+++ chapter_5/5_1_mach_trap.d
@@ -0,0 +1,32 @@
+#!/usr/sbin/dtrace -s
+
+
+mach_trap::clock_sleep_trap:entry{
+  self->tracing = 1;
+  printf("file at: %x opened with mode %x", arg0, arg1);
+}
+
+fbt:::entry
+/ self->tracing /
+{
+  printf("%x %x %x",arg0, arg1, arg2);
+}
+
+fbt::open:entry
+/ self->tracing /
+{ 
+  printf("PID %d (%s) is opening \n", ((proc_t)arg0)->p_pid, ((proc_t)arg0)->p_comm);
+}
+
+fbt:::return 
+/ self->tracing /
+{
+  printf("Returned %x\n",arg1);
+}
+
+mach_trap::clock_sleep_trap:return
+/ self->tracing / 
+{
+  self->tracing=0;
+  exit(0)
+}
